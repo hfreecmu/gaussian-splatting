ab71ae83025fc6fc653a4b8eae3c10bc2063eb12
hfreeman <hfreeman@andrew.cmu.edu>
2025-03-31 10:11:29 -0400

    small cange lab gpu

--- utils/graphics_utils.py
+++ utils/graphics_utils.py
@@ -70,6 +70,7 @@ def getProjectionMatrix(znear, zfar, fovX, fovY, cx, cy):
     P[3, 2] = z_sign
     P[2, 2] = z_sign * zfar / (zfar - znear)
     P[2, 3] = -(zfar * znear) / (zfar - znear)
+
     return P
 
 def fov2focal(fov, pixels):
